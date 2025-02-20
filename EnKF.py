babca8827c854db4f9987be1f46ba8c188aaad1c
Fatih Erden <fatih.erden@metu.edu.tr>
2025-02-20 18:56:32 +0300

    Initialize Python EnKF repo

---
+++ EnKF.py
@@ -0,0 +1,134 @@
+# Based on the filterpy EnKF
+# https://filterpy.readthedocs.io/en/latest/_modules/filterpy/kalman/ensemble_kalman_filter.html#EnsembleKalmanFilter.update
+# Book: https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from copy import deepcopy
+import numpy as np
+from numpy import array, zeros, eye, dot
+from numpy.random import multivariate_normal
+from filterpy.common import outer_product_sum
+
+
+class EnKF(object):
+
+    def __init__(self, x, P, dim_z, dt, N, fx, filename, last_time_instant, con_param_index):
+
+        # initialization of ensemble kalman filter
+
+        dim_x = len(x)
+        self.dim_x = dim_x  # equals to total state number
+        self.dim_z = dim_z  # equals to 2 (P,Q)
+        self.dt = dt  # delta time
+        self.N = N  # number of ensemble
+        self.WPE_fx = fx  # state transition function, note that this function is only for the states. the transition function for the parameters are identity.
+        self.K = zeros((dim_x, dim_z))
+        self.z = array([[None] * self.dim_z]).T
+        self.S = zeros((dim_z, dim_z))  # system uncertainty
+        self.SI = zeros((dim_z, dim_z))  # inverse system uncertainty
+
+        self.initialize(x, P)
+
+        self.Q = eye(dim_x) * 0.000001  # process uncertainty
+        self.R = eye(dim_z) * 0.0001  # measurement uncertainty 
+        self.inv = np.linalg.inv
+
+        self._mean = zeros(dim_x)
+        self._mean_z = zeros(dim_z)
+
+        self.filename = filename  # last time instant dynamics snapshot file.
+        self.last_time_instant = last_time_instant  # last time instant
+        self.con_param_index = con_param_index  # indexes of parameters to be estimated
+
+    def initialize(self, x, P):
+
+        if x.ndim != 1:
+            raise ValueError('x must be a 1D array')
+
+        self.sigmas = multivariate_normal(mean=x, cov=P,
+                                          size=self.N)  # create sigma values of mean x, covariance P and size (state_number x N)
+
+        self.x = x
+        self.P = P
+
+        # these will always be a copy of x,P after predict() is called
+        self.x_prior = self.x.copy()
+        self.P_prior = self.P.copy()
+
+        # these will always be a copy of x,P after update() is called
+        self.x_post = self.x.copy()
+        self.P_post = self.P.copy()
+
+    def predict(self):
+        """ Predict next position. """
+        # Transition k --> k+1
+
+        N = self.N
+        self.sigmas_h = zeros((N, self.dim_z))  # z
+
+        # The sigma values are transitioned into the next time instant
+        # with the help of WPE_fx function. This is done sequentially for every sigma (every state vector).
+        for i, s in enumerate(self.sigmas):
+            filename = 'snap/ensemble_' + str(i)
+            basecase_name = 'snap/ensemble_' + str(i)
+            self.sigmas_h[i], self.sigmas[i] = self.WPE_fx(s, self.dt, filename, self.last_time_instant,
+                                                          self.con_param_index, basecase_name)
+
+        e = multivariate_normal(self._mean, self.Q, N)
+        self.sigmas += e
+
+        self.P = outer_product_sum(self.sigmas - self.x) / (N - 1) #+1
+
+        # save prior
+        self.x_prior = np.copy(self.x)
+        self.P_prior = np.copy(self.P)
+
+    def update(self, z, R=None):
+
+        """ Update according to the measurements (if any) """
+        # The sigma values are updated by x_k+1 (+)  = x_k+1 (-) + K*(z_k+1-h(x_k+1)).
+
+        if z is None:
+            self.z = array([[None] * self.dim_z]).T
+            self.x_post = self.x.copy()
+            self.P_post = self.P.copy()
+            return
+
+        if R is None:
+            R = self.R
+            self.R[0,0] = 0.00001 # P_meas uncertainty
+            self.R[1,1] = 0.00001 #Q_meas uncertainty
+        if np.isscalar(R):
+            R = eye(self.dim_z) * R
+
+        N = self.N
+        sigmas_h = self.sigmas_h
+
+        # Take the mean or median of the output predictions.
+        z_mean = np.mean(sigmas_h, axis=0)
+        # z_mean = np.nanmedian(sigmas_h, axis=0)
+
+        P_zz = (outer_product_sum(sigmas_h - z_mean) / (N - 1)) + R #+1
+        P_xz = outer_product_sum(
+            self.sigmas - self.x, sigmas_h - z_mean) / (N - 1) #+1
+
+        self.S = P_zz
+        self.SI = self.inv(self.S)
+        self.K = dot(P_xz, self.SI)
+
+        e_r = multivariate_normal(self._mean_z, R, N)
+        for i in range(N):
+            self.sigmas[i] += dot(self.K, z + e_r[i] - sigmas_h[i])
+
+        # Take the mean or median of the states.
+        # self.x = np.mean(self.sigmas, axis=0)
+        self.x = np.nanmedian(self.sigmas, axis=0)
+
+        self.P = self.P - dot(dot(self.K, self.S), self.K.T)
+
+        # save measurement and posterior state
+        self.z = deepcopy(z)
+        self.x_post = self.x.copy()
+        self.P_post = self.P.copy()
+
---
+++ model_functions.py
@@ -0,0 +1,246 @@
+# This file contains the functions that are used to calculate the network equations and 
+# the state derivatives for the synchronous machine, exciter, and governor models.
+import numpy as np
+import matplotlib.pyplot as plt
+
+def network_solver(V, theta, x, y, SM_params):
+    # Calculate the network equations and return the algebraic variables
+    # x         : State vector
+    # y         : Algebraic variables vector
+    # SM_params : Synchoronous machine model parameters
+
+    # Unpack the state variables
+    delta = x[2]
+    Ed = x[1]
+    Eq = x[0]
+
+    # Unpack the parameters
+    Xd1, Xq1 = SM_params[3], SM_params[5]
+
+    Vd = V * np.sin(delta - theta)
+    Vq = V * np.cos(delta - theta)
+
+    Id = (Eq - Vq) / Xd1
+    Iq = (Vd-Ed) / Xq1
+
+    Pe = Vd * Id + Vq * Iq
+    Qe = Vq * Id - Vd * Iq
+
+    # Pack the algebraic variables into the y vector
+    y[0] = Vd
+    y[1] = Vq
+    y[2] = Id
+    y[3] = Iq
+    y[4] = Pe
+    y[5] = Qe
+
+    return y
+
+def model_init(V, theta, delta, Efd, SM_params, EX_params):
+    # Model will be initialized with the given parameters to steady state.
+    # V         : Voltage magnitude at POI
+    # theta     : Voltage angle at POI
+    # delta     : Initial angle of the generator
+    # Efd       : Initial excitation voltage
+    # SM_params : Synchoronous machine model parameters
+    # EX_params : IEEE DC1A Exciter model parameters
+    # No need for GO_params.
+
+    # Initialize state, input and algebraic variable vectors
+    states = np.zeros(9)
+    u = np.zeros(2)
+    y = np.zeros(6)
+
+    # Calculate the KE value
+    KE = -EX_params[6]*np.exp(EX_params[7]*Efd)
+
+    # First assign the initial conditions for delta (s3) and Efd (s5)
+    states[2] = delta
+    states[4] = Efd      
+
+    Vd = V*np.sin(delta - theta)
+    Vq = V*np.cos(delta - theta)
+
+    # Unpack the reactances
+    Xd, Xd1, Xq, Xq1 = SM_params[2], SM_params[3], SM_params[4], SM_params[5]
+
+    Id = (Efd - Vq) / Xd
+    Iq = (Vd) / Xq
+
+    Pe = Vd * Id + Vq * Iq
+    Qe = Vq * Id - Vd * Iq    
+
+    # Calculate the initial conditions for the rest of the states
+    states[0] = Vq + Xd1*Id    # Eq
+    states[1] = (Xq-Xq1)*Iq                   # Ed
+    states[3] = 1                                 # w        
+    states[5] = 0                                 # VF
+    states[6] = 0                                 # VR
+    states[7] = Pe                                # TM
+    states[8] = Pe                                # Psv
+
+    # Calculate the algebraic variables
+    y[0] = Vd                                    # Vd
+    y[1] = Vq                                    # Vq
+    y[2] = Id                                    # Id
+    y[3] = Iq                                    # Iq
+    y[4] = Pe                                    # Pe
+    y[5] = Qe                                    # Qe
+
+    # Calculate and pack the input vector
+    u[0] = V                                      # Vref
+    u[1] = Pe                                     # Pc
+
+    # Check the initial power values
+    print("Initial Pe: ", y[4], "-- Initial Qe: ", y[5])
+
+    return states, y, u, KE
+
+def fx(states, u, y, SM_params, EX_params, GO_params, dt):
+    # Calculate the state derivatives and update the state vector for the next time step
+    # states : Current state vector
+    # u      : Input vector
+    # y      : Algebraic variables
+    # dt     : Time step
+
+    # Unpack the states
+    Eq, Ed, delta, w, Efd, VF, VR, TM, Psv = states
+
+    # Unpack the inputs
+    Vref, Pc = u
+
+    # Unpack the algebraic variables
+    Vd, Vq, Id, Iq, Pe, Qe = y
+
+    V = np.sqrt(Vd**2 + Vq**2)
+    # Unpack the parameters
+    Tdo, Tqo, Xd, Xd1, Xq, Xq1, H, D = SM_params
+    TE, KE, TF, KF, TA, KA, A, B = EX_params
+    TCH, TSV, RD = GO_params
+
+    SE = A*np.exp(B*Efd)
+
+    # Calculate the derivatives
+    dEq = (-Eq -(Xd-Xd1)*Id + Efd) / Tdo
+    dEd = (-Ed +(Xq-Xq1)*Iq) / Tqo
+    dDelta = w - 1                              # ws = 1    
+    dW = (TM - Pe - D*(w-1)) / (2*H)
+    dEfd = (-(KE+SE)*Efd + VR) / TE
+    dVF = (-VF + KF*VR/TE - KF*(KE+SE)*Efd/TE) / TF
+    dVR = (-VR + KA*(Vref-VF-V)) / TA
+    dTM = (Psv - TM) / TCH
+    dPsv = (-Psv + Pc - (w-1)/RD) / TSV
+
+    # Update the states
+    # Apply  euler method for the numerical solution
+    # x_k+1 = x_k + dt*F(x_k)
+    states[0] += dEq * dt
+    states[1] += dEd * dt
+    states[2] += dDelta * dt
+    states[3] += dW * dt
+
+    exc_disable = False
+    if exc_disable:
+        states[4] = Efd
+    else:
+        states[4] += dEfd * dt
+    
+    states[5] += dVF * dt
+    states[6] += dVR * dt
+
+    gov_disable = False
+    if gov_disable:
+        states[7] = TM
+    else:
+        states[7] += dTM * dt
+    
+    states[8] += dPsv * dt
+
+    return states
+
+def model_init2(Pt, Qt, V, SM_params, EX_params):
+    # Model will be initialized with the given machine operation to steady state. REF:Kundur
+    # Pt        : Real power at POI
+    # Qt        : Reactive power at POI
+    # V         : Voltage magnitude at POI
+    # SM_params : Synchoronous machine model parameters
+    # EX_params : IEEE DC1A Exciter model parameters
+    # No need for GO_params.
+
+    # Unpack the synchronous machine parameters
+    Xd, Xd1, Xq, Xq1 = SM_params[2], SM_params[3], SM_params[4], SM_params[5]
+
+    # Calculate the terminal current.
+    S = np.sqrt(Pt**2 + Qt**2)
+    I = S / V
+    phi = np.arccos(Pt / S)*np.sign(Qt)
+
+    # Calculate the internal rotor angle
+    delta = np.arctan((Xq*I*np.cos(phi))/(V + Xq*I*np.sin(phi)))
+
+    # Calculate the terminal dq components
+    Vd = V*np.sin(delta)
+    Vq = V*np.cos(delta)
+    Id = I*np.sin(phi + delta)
+    Iq = I*np.cos(phi + delta)
+
+    # Calculate the internal dq voltages
+    Ed = Vd - Xq1*Iq
+    Eq = Vq + Xd1*Id
+
+    # Calculate the initial excitation voltage
+    Efd = Eq + (Xd - Xd1)*Id
+
+    # Initial mechanical power equals the terminal power
+    Pe = Pt
+    Tm = Pe
+
+    # Calculate the initial KE value
+    KE = -EX_params[6]*np.exp(EX_params[7]*Efd)
+
+    # Initialize the state, input and algebraic variable vectors
+    states = np.zeros(9)
+    u = np.zeros(2)
+    y = np.zeros(6)
+
+    # Assign the initial conditions of the states
+    states[0] = Eq
+    states[1] = Ed
+    states[2] = delta
+    states[3] = 1
+    states[4] = Efd
+    states[5] = 0
+    states[6] = 0
+    states[7] = Tm
+    states[8] = Tm
+
+    # Calculate the algebraic variables
+    y[0] = Vd
+    y[1] = Vq
+    y[2] = Id
+    y[3] = Iq
+    y[4] = Pe
+    y[5] = Qt
+
+    # Calculate the input vector
+    u[0] = V
+    u[1] = Pe
+    
+    return states, y, u, KE
+
+def plotter(data_hist,index,name, time_vector):
+    # Plot the simulation results
+    # data_hist : Simulation results
+    # index     : Index of the variable to be plotted
+    # name      : Name of the variable to be plotted
+
+    ydata = data_hist[:,index]
+    plt.figure()
+    plt.plot(time_vector, ydata)
+    plt.xlabel('Time (s)')
+    plt.ylabel(name)
+    plt.title(name + ' vs Time')
+    #plt.grid()
+    #plt.show()
+    plt.savefig(name + '.png')
+    return
